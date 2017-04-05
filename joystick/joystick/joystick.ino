3611f34b18740b5c4256a5e3ccd6d17813776ab2
Meagan <peatm@sheridanc.on.ca>
2017-04-05 12:44:26 -0400

    got direction marking started

--- joystick/joystick/joystick.ino
+++ joystick/joystick/joystick.ino
@@ -1,33 +1,71 @@
-int xPin = A1;
-int yPin = A0;
-int buttonPin = 2;
+const int joyStickXPin = A1;
+const int joyStickYPin = A0;
+const int joyStickButtonPin = 2;
 
-int xPosition = 0;
-int yPosition = 0;
-int buttonState = 0;
+int joyStickXPosition = 0;
+int joyStickYPosition = 0;
+int joyStickButtonState = 0;
+
+//0 = center, 1 = left/up, 2 = right/down
+int joyStickXLast = 0;
+int joyStickYLast = 0;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600); 
   
-  pinMode(xPin, INPUT);
-  pinMode(yPin, INPUT);
+  pinMode(joyStickXPin, INPUT);
+  pinMode(joyStickYPin, INPUT);
 
   //activate pull-up resistor on the push-button pin
-  pinMode(buttonPin, INPUT_PULLUP); 
+  pinMode(joyStickButtonPin, INPUT_PULLUP); 
 }
 
 void loop() {
-  xPosition = analogRead(xPin);
-  yPosition = analogRead(yPin);
-  buttonState = digitalRead(buttonPin);
-  
-  Serial.print("X: ");
-  Serial.print(xPosition);
-  Serial.print(" | Y: ");
-  Serial.print(yPosition);
-  Serial.print(" | Button: ");
-  Serial.println(buttonState);
+  joyStickXPosition = analogRead(joyStickXPin);
+  joyStickYPosition = analogRead(joyStickYPin);
+  joyStickButtonState = digitalRead(joyStickButtonPin);
 
-  delay(500);
+  if(joyStickXPosition >= 700){
+    if(joyStickXLast != 1){
+      joyStickXLast = 1;
+      Serial.println("up "); 
+    } 
+  } else if (joyStickXPosition <= 100){
+    if(joyStickXLast != 2){
+      joyStickXLast = 2;
+      Serial.println("down ");
+    }
+  }else{
+    joyStickXLast = 0;
+  }
+
+
+  if(joyStickYPosition >= 700){
+    if(joyStickYLast != 1){
+      joyStickYLast = 1;
+      Serial.println("left"); 
+    } 
+  } else if (joyStickYPosition <= 100){
+    if(joyStickYLast != 2){
+      joyStickYLast = 2;
+      Serial.println("right");
+    }
+  }else{
+    joyStickYLast = 0;
+  }
+
+ 
+
+
+
+  
+//  Serial.print("X: ");
+//  Serial.print(joyStickXPosition);
+//  Serial.print(" | Y: ");
+//  Serial.print(joyStickYPosition);
+//  Serial.print(" | Button: ");
+//  Serial.println(joyStickButtonState);
+
+  delay(100);
 }
