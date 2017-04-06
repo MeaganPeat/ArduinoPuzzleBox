12d08cce85df66b033c979982d327ded67f441ae
Meagan <peatm@sheridanc.on.ca>
2017-04-06 14:24:09 -0400

    starting again

---
+++ puzzleCode/puzzleCode.ino
@@ -0,0 +1,21 @@
+//sets pin numbers
+const int joyStickXPin = A1;
+const int joyStickYPin = A0;
+const int joyStickButtonPin = 2;
+const int touchPin = 6
+const int resetBtn = 7;
+const int tiltPin = 8;
+const int trigPin = 9;
+const int echoPin = 10;
+const int lights[12] = {0, 1, 3, 4, 5, 11, 12, 13, 16, 17, 18, 19 }
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
